53936f9cbb106b26249d57f2def5c4ab6bbf40f0
Passiflore <elise.vue@hetic.net>
2023-11-08 09:58:44 +0100

    Add 1 to 10 range

--- RomanNumbers.js
+++ RomanNumbers.js
@@ -5,52 +5,40 @@ function mutateNumberToRoman(number) {
     if (number >= 1000) {
       result = result + "M".repeat(number / 1000);
       number = number % 1000;
-    } 
-    else if (number >= 900) {
+    } else if (number >= 900) {
       result = result + "CM";
       number = number % 900;
-    } 
-    else if (number >= 500) {
+    } else if (number >= 500) {
       result = result + "D";
       number = number % 500;
-    } 
-    else if (number >= 400) {
+    } else if (number >= 400) {
       result = result + "CD";
       number = number % 400;
-    } 
-    else if (number >= 100) {
+    } else if (number >= 100) {
       result = result + "C".repeat(number / 100);
       number = number % 100;
-    } 
-    else if (number >= 90) {
+    } else if (number >= 90) {
       result = result + "XC";
       number = number % 90;
-    } 
-    else if (number >= 50) {
+    } else if (number >= 50) {
       result = result + "L";
       number = number % 50;
-    } 
-    else if (number >= 40) {
+    } else if (number >= 40) {
       result = result + "XL";
       number = number % 40;
-    } 
-    else if (number >= 10) {
+    } else if (number >= 10) {
       result = result + "X".repeat(number / 10);
       number = number % 10;
-    } 
-    else if (number >= 9) {
+    } else if (number >= 9) {
       result = result + "IX";
       number = number % 9;
-    } 
-    else if (number >= 5) {
+    } else if (number >= 5) {
       result = result + "V";
       number = number % 5;
-    } 
-    else if (number >= 4) {
+    } else if (number >= 4) {
       result = result + "IV";
       number = number % 4;
-    } 
-    else if (number >= 1) {
+    } else if (number >= 1) {
       result = result + "I".repeat(number / 1);
       number = number % 1;
     }
