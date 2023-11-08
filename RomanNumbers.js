b464544adeddf8965d4e5507a8ef99a569123951
Passiflore <elise.vue@hetic.net>
2023-11-08 09:58:44 +0100

    Add 1 to 10 range

---
+++ RomanNumbers.js
@@ -0,0 +1,43 @@
+function mutateNumberToRoman(number) {
+  let result = "";
+
+  while (number > 0) {
+    if (number >=10){
+      result = result + "X".repeat(number/10); 
+      number = number%10;
+
+    }
+    else if (number>=9){
+      result = result + "IX";
+      number = number%9;
+
+    } 
+    else if (number>=5){
+      result = result + "V";
+      number = number%5;
+
+    } 
+    else if (number>=4){
+      result = result + "IV";
+      number = number%4;
+
+    }
+    else if (number>=1){
+      result = result + "I".repeat(number/1);
+      number = number%1;
+    }
+  }
+  console.log(result)
+}
+
+function main([arg]) {
+  if (!isNaN(arg)) {
+    const number = Number(arg);
+    mutateNumberToRoman(number);
+  } else {
+    console.error("Please provide me a number");
+  }
+}
+
+main(process.argv.slice(2));
+
