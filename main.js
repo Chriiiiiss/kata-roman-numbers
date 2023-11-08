1f0fefe6ef2d88dac2c0b687550eb79f652c46de
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2023-11-08 11:03:57 +0100

    Manage Error and Add the logic up to 1000 (#9)
    * Remove useless things
    
    * [ADD] add jest and remove useless file
    
    * [ADD] Add tests up to 10
    
    * Add 1 to 10 range
    
    * Change console log to return
    
    * [ADD] Add tests up to 1000
    
    * [ADD] Add main function back
    
    * remove main from RomanNumbers.js
    
    * Add 1 to 1000 range and correct test (#8)
    
    Co-authored-by: Passiflore <elise.vue@hetic.net>
    
    * Manage 0 and float (#7)
    
    ---------
    
    Co-authored-by: Passiflore <elise.vue@hetic.net>

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const mutateNumberToRoman = require("./RomanNumbers");
 
 function main([arg]) {
+  if (!Number.isInteger(Number(arg)) || Number(arg) === 0) {
+    console.log(`Roman didn't invented 0 and float number`);
+    return -1;
+  }
+
   if (!isNaN(arg)) {
     const number = Number(arg);
     const romanNumber = mutateNumberToRoman(number);
     console.log(`The Roman Number of ${number} is ${romanNumber}`);
+    return 1;
   } else {
     console.error("Please provide me a number");
+    return -1;
   }
 }
 
