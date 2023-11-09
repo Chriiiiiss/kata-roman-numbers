e6e39166760c468688e2faee0ee5a7ad64848ac0
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2023-11-09 01:57:14 +0100

    [ADD] Add revert tests (#17)
    * [ADD] New Tests
    
    * [ADD] Linted

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+/// <reference types="vitest"/>
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   plugins: [react()],
   base: "",
 });
